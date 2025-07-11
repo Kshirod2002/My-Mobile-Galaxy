fd60ed979a1c64b4bcb2e43bae355de391ac6f7e
Kshirod  Chandra Swain <swainkunal164@gmail.com>
2025-07-11 11:42:14 +0530

    Update accessory.jsx

--- frontend/src/Components/Products/accessory.jsx
+++ frontend/src/Components/Products/accessory.jsx
@@ -63,7 +63,7 @@ const Accessory = () => {
     };
 
     try {
-      const res = await axios.post("http://localhost:8081/add-to-cart", {
+      const res = await axios.post("https://my-mobile-galaxy.onrender.com/add-to-cart", {
         userId,
         product: productToSend,
       });
